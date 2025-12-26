1393d2cead2174248ae58bf0523a8fc3e591a0af
Diego Acioli <164804853+dglins@users.noreply.github.com>
2025-12-26 03:56:52 +0000

    Refactor repeat_alpha.c to simplify character repetition

--- level-0/repeat_alpha.c
+++ level-0/repeat_alpha.c
@@ -9,56 +9,31 @@
 
 #include <unistd.h>
 
-/*
-** Função que calcula quantas vezes uma letra deve ser repetida
-** baseado em sua posição no alfabeto
-**
-** Retorno:
-**   Para 'a' ou 'A': 1
-**   Para 'b' ou 'B': 2
-**   Para 'z' ou 'Z': 26
-*/
-int	get_repeat_count(char c)
+int main(int argc, char **argv)
 {
-	// Se for minúscula, retorna posição no alfabeto (a=1, b=2, etc.)
-	if (c >= 'a' && c <= 'z')
-		return (c - 'a' + 1);
-	
-	// Se for maiúscula, retorna posição no alfabeto (A=1, B=2, etc.)
-	if (c >= 'A' && c <= 'Z')
-		return (c - 'A' + 1);
-	
-	// Se não for letra, repete apenas 1 vez
-	return (1);
-}
+  int i;
+  char c;
+  int times;
 
-int	main(int argc, char **argv)
-{
-	int	i;
-	int	repeat;
-	int	j;
-
-	// Verifica se há exatamente 1 argumento
-	if (argc == 2)
-	{
-		i = 0;
-		while (argv[1][i] != '\0')
-		{
-			// Calcula quantas vezes o caractere deve ser repetido
-			repeat = get_repeat_count(argv[1][i]);
-			
-			// Imprime o caractere 'repeat' vezes
-			j = 0;
-			while (j < repeat)
-			{
-				write(1, &argv[1][i], 1);
-				j++;
-			}
-			i++;
-		}
-	}
-	
-	// Sempre imprime newline no final
-	write(1, "\n", 1);
-	return (0);
+  if (argc == 2)
+  {
+    i = 0;
+    while (argv[1][i])
+    {
+      c = argv[1][i];
+      times = 1;
+      if (c >= 'a' && c <= 'z')
+        times = c - 'a' + 1;
+      else if (c >= 'A' && c <= 'Z')
+        times = c - 'A' + 1;
+      while (times > 0)
+      {
+        write(1, &c, 1);
+        times--;
+      }
+      i++;
+    }
+  }
+  write(1, "\n", 1);
+  return (0);
 }
