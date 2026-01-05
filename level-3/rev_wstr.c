8b46fb76bc71c06f0ac22f6cb14f28313b520d96
Diego Acioli <164804853+dglins@users.noreply.github.com>
2026-01-05 02:05:00 +0000

    Improve argument validation and reverse string logic
    Refactor argument check and loop structure for clarity.

--- level-3/rev_wstr.c
+++ level-3/rev_wstr.c
@@ -14,27 +14,23 @@ int	main(int argc, char **argv)
 	int i;
 	int end;
 
-	if (argc != 2)
+	if (argc == 2)
 	{
-		write(1, "\n", 1);
-		return (0);
-	}
-
-	i = 0;
-	while (argv[1][i])
-		i++;
-	end = i;
-
-	while (i >= 0)
-	{
-		if (i == 0 || argv[1][i - 1] == ' ')
+		i = 0;
+		while (argv[1][i])
+			i++;
+		end = i;
+		while (i >= 0)
 		{
-			write(1, &argv[1][i], end - i);
-			if (i > 0)
-				write(1, " ", 1);
-			end = i - 1;
+			if (i == 0 || argv[1][i - 1] == ' ')
+			{
+				write(1, &argv[1][i], end - i);
+				if (i > 0)
+					write(1, " ", 1);
+				end = i - 1;
+			}
+			i--;
 		}
-		i--;
 	}
 	write(1, "\n", 1);
 	return (0);
