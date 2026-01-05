bc9ebccf4fbd3b79c5984769d2d58fb741545c84
Diego Acioli <164804853+dglins@users.noreply.github.com>
2026-01-05 02:03:43 +0000

    Refactor rev_wstr.c to simplify main logic

--- level-3/rev_wstr.c
+++ level-3/rev_wstr.c
@@ -7,54 +7,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-  	 #include <stdlib.h>
-     #include <unistd.h>
+#include <unistd.h>
 
-     static int is_space(char c) {
-       return (c == ' ' || (c >= 9 && c <= 13));
-     }
-
-     static int skip_spaces_left(const char *s, int i) 
+int	main(int argc, char **argv)
 {
-       while (i >= 0 && is_space(s[i])) 
-		   i--;
-       return i;
-     }
+	int i;
+	int end;
 
-     static int skip_word_left(const char *s, int i) 
-{
-       while (i >= 0 && !is_space(s[i])) 
-		   i--;
-       return i;
-     }
+	if (argc != 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
 
-     static void write_word(const char *s, int start, int end) 
-{
-       while (start <= end) 
-		   write(1, &s[start++], 1);
-     }
+	i = 0;
+	while (argv[1][i])
+		i++;
+	end = i;
 
-     int main(int argc, char **argv) 
-{
-       if (argc == 2) {
-         int i = 0;
-         while (argv[1][i]) 
-			 i++;
-         while (i >= 0) 
-		 {
-           i = skip_spaces_left(argv[1], i);
-           if (i < 0) 
-			   break;
-           int start = skip_word_left(argv[1], i) + 1;
-           write_word(argv[1], start, i);
-           i = skip_word_left(argv[1], i);
-           i = skip_spaces_left(argv[1], i);
-           if (i >= 0) 
-			   write(1, " ", 1);
-		   i--;
-         }
-       }
-       write(1, "\n", 1);
-       return 0;
-     }
+	while (i >= 0)
+	{
+		if (i == 0 || argv[1][i - 1] == ' ')
+		{
+			write(1, &argv[1][i], end - i);
+			if (i > 0)
+				write(1, " ", 1);
+			end = i - 1;
+		}
+		i--;
+	}
+	write(1, "\n", 1);
+	return (0);
+}
 
