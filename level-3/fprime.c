e7b37865e38b0447c3998f14bae2a314d465ed1b
Diego Acioli <164804853+dglins@users.noreply.github.com>
2026-01-05 13:27:33 +0000

    Refactor prime factorization logic in fprime.c

--- level-3/fprime.c
+++ level-3/fprime.c
@@ -10,58 +10,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char *argv[])
 {
-	int	number;
-	int	divisor;
-	int	first;
+	if (argc == 2)
+	{
+		int	num = atoi(argv[1]);
+		int	i = 2;
 
-	// Verifica se há exatamente 1 argumento
-	if (argc != 2)
-	{
-		printf("\n");
-		return (0);
-	}
-
-	// Converte o argumento para inteiro
-	number = atoi(argv[1]);
-	
-	// Caso especial: número 1
-	if (number == 1)
-	{
-		printf("1");
-	}
-	
-	// Inicia com o menor divisor primo (2)
-	divisor = 2;
-	first = 1;  // Flag para controlar a impressão do '*'
-	
-	// Enquanto o número for maior que 1, procura fatores primos
-	while (number > 1)
-	{
-		// Se o divisor atual divide o número
-		if (number % divisor == 0)
+		if (num == 1)
+			printf("1");
+		while (i <= num)
 		{
-			// Imprime o separador '*' se não for o primeiro fator
-			if (first == 0)
+			if (num % i == 0)
+			{
+				printf("%d", i);
+				if (num == i)
+					break;
 				printf("*");
-			
-			// Imprime o fator primo
-			printf("%d", divisor);
-			
-			// Divide o número pelo fator encontrado
-			number = number / divisor;
-			
-			// Marca que já imprimimos o primeiro fator
-			first = 0;
-		}
-		else
-		{
-			// Se não divide, tenta o próximo divisor
-			divisor++;
+				num /= i;
+			}
+			else
+				i++;
 		}
 	}
-	
 	printf("\n");
-	return (0);
 }
