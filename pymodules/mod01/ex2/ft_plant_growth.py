f26bbc25e0abd52f06a7c5d73c480eba38b66546
dglins <diegoacioli@gmail.com>
2026-01-21 19:08:02 +0000

    fix: ex0 ex1 ex2 trying to implement what is asked at the subject

--- pymodules/mod01/ex2/ft_plant_growth.py
+++ pymodules/mod01/ex2/ft_plant_growth.py
@@ -1,49 +1,52 @@
 #!/usr/bin/env python3
-"""Module to register plants and simulate growth."""
+"""Module to simulate growth."""
 
 
 class Plant:
     """Represents a plant with name, height (cm), and age (days)."""
 
-    def __init__(self, name: str, height: int, age: int) -> None:
+    def __init__(self, name: str, initial_height: int, initial_age: int) -> None:
         """Initialize plant."""
         self.name: str = name
-        self.height: int = height
-        self.age: int = age
+        self.initial_height: int = initial_height
+        self.initial_age: int = initial_age
 
     def grow(self, cm: int = 1) -> None:
         """Increse height by cm"""
-        self.height += cm
+        self.initial_height += cm
 
-    def age_one_day(self, days: int = 1) -> None:
+    def age(self, days: int = 1) -> None:
         """Incrise age by days"""
-        self.age += days
+        self.initial_age += days
 
     def day_pass(self) -> None:
         """Advance one day: grow 1 cm and age 1 day"""
         self.grow(1)
-        self.age_one_day(1)
+        self.age(1)
 
-    def get_info(self) -> str:
+    def get_info(self) -> dict[str, str]:
+        """Return informations of plant."""
+        return self.__dict__
+
+    def __str__(self) -> str:
         """Return representation of plant."""
-        return f"{self.name}: {self.height}cm, {self.age} days old"
+        return f"{self.name}: {self.initial_height}cm, {self.initial_age} days old"
 
-
-def grow_days(plant: Plant, days: int = 7) -> None:
-    """Advance the plant by a number of days (default: 7)."""
-    for _ in range(days - 1):
-        plant.day_pass()
+    def grow_days(self, days: int = 7) -> None:
+        """Advance the plant by a number of days (default: 7)."""
+        for _ in range(days - 1):
+            self.day_pass()
 
 
 def run_cli(plant: Plant, days_to_grow: int = 7) -> None:
     """Creates a plant, simulates a week, and shows before/after."""
     print("\n\n=== Day 1 ===")
-    print(plant.get_info())
-    initial_hight: int = plant.height
-    grow_days(plant, days_to_grow)
+    print(plant)
+    init_height: int = plant.initial_height
+    plant.grow_days(days_to_grow)
     print(f"=== Day {days_to_grow} ===")
-    print(plant.get_info())
-    print(f"Growth this week: +{plant.height - initial_hight}cm")
+    print(plant)
+    print(f"Growth this week: +{plant.initial_height - init_height}cm")
 
 
 def main() -> None:
